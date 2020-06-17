b36467057a7f74405452da405953e2993a58279a
Sneha Sil <snehasil413@gmail.com>
2020-06-17 10:04:06 -0500

    edited arraylist

--- IntegerManip/IntegerManip/Program.cs
+++ IntegerManip/IntegerManip/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Schema;
@@ -32,6 +33,47 @@ namespace IntegerManip
                 cont = Convert.ToInt32(input);
                 total++;
             }
+            int cont2 = 1;
+            while (cont2 != 7)
+            {
+                Console.WriteLine("1. Display all numbers");
+                Console.WriteLine("2. Display every even number");
+                Console.WriteLine("3. Display every odd number");
+                Console.WriteLine("4. Display every 5th number");
+                Console.WriteLine("5. Display the sum of the integers");
+                Console.WriteLine("6. Display every number divisible by 3");
+                Console.WriteLine("7. Quit");
+                int menuoption = Convert.ToInt32(Console.ReadLine());
+                if (menuoption == 1)
+                {
+                    for (int counter = 0; counter < numbers.Count; counter++)
+                    {
+                        Console.Write(numbers.ElementAt(counter) + " ");
+                    }
+                    Console.ReadLine();
+                }
+                if (menuoption == 2)
+                {
+                    evenNumbers();
+                }
+                if (menuoption == 3)
+                {
+                    OddNumbers();
+                }
+                if (menuoption == 4)
+                {
+                    Every5th();
+                }
+                if (menuoption == 5)
+                {
+                    numberSum();
+                }
+                if (menuoption == 6)
+                {
+                    DivisibleBy3();
+                }
+            }
+         
         }
     }
 }
